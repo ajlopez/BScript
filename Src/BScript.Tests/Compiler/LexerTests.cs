00c478fcfe258cbf1876db758eda366c8093ea69
ajlopez <webmaster@ajlopez.com>
2014-12-31 11:27:29 -0300

    Lexer get name

---
+++ Src/BScript.Tests/Compiler/LexerTests.cs
@@ -0,0 +1,27 @@
+﻿namespace BScript.Tests.Compiler
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BScript.Compiler;
+
+    [TestClass]
+    public class LexerTests
+    {
+        [TestMethod]
+        public void GetName()
+        {
+            Lexer lexer = new Lexer("foo");
+
+            var token = lexer.NextToken();
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual(TokenType.Name, token.Type);
+            Assert.AreEqual("foo", token.Value);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+    }
+}
