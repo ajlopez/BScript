2544d216b6c042416bbf44bde9c5f718214e0046
ajlopez <webmaster@ajlopez.com>
2015-01-06 05:24:37 -0300

    Constant expression first test

---
+++ Src/BScript.Tests/Expressions/ConstantExpressionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace BScript.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BScript.Expressions;
+
+    [TestClass]
+    public class ConstantExpressionTests
+    {
+        [TestMethod]
+        public void CreateIntegerConstant()
+        {
+            var expr = new ConstantExpression(42);
+
+            Assert.AreEqual(42, expr.Value);
+            Assert.AreEqual(42, expr.Evaluate());
+        }
+    }
+}
