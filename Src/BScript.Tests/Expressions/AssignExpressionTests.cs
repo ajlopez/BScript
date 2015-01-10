e8fb88277a2d8e4ba0130f027308f6278c085d21
ajlopez <webmaster@ajlopez.com>
2015-01-10 07:19:17 -0300

    Assign expression

---
+++ Src/BScript.Tests/Expressions/AssignExpressionTests.cs
@@ -0,0 +1,30 @@
+﻿namespace BScript.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BScript.Expressions;
+
+    [TestClass]
+    public class AssignExpressionTests
+    {
+        [TestMethod]
+        public void AssignInteger()
+        {
+            Context context = new Context();
+            IExpression expr = new ConstantExpression(42);
+            AssignExpression aexpr = new AssignExpression("a", expr);
+
+            Assert.AreEqual("a", aexpr.Name);
+            Assert.AreSame(expr, aexpr.Expression);
+
+            var result = aexpr.Evaluate(context);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(42, result);
+            Assert.AreEqual(42, context.GetValue("a"));
+        }
+    }
+}
