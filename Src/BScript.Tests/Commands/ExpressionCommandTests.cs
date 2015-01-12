365cb8e1f0e32b9cf5c2662202a4fd81abfb68b9
ajlopez <webmaster@ajlopez.com>
2015-01-12 07:05:48 -0300

    ExpressionCommand

---
+++ Src/BScript.Tests/Commands/ExpressionCommandTests.cs
@@ -0,0 +1,36 @@
+﻿namespace BScript.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Commands;
+    using BScript.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ExpressionCommandTests
+    {
+        [TestMethod]
+        public void CreateExpressionCommand()
+        {
+            IExpression expr = new ConstantExpression(1);
+            ExpressionCommand cmd = new ExpressionCommand(expr);
+
+            Assert.IsNotNull(cmd.Expression);
+            Assert.AreSame(expr, cmd.Expression);
+        }
+
+        [TestMethod]
+        public void ExecuteExpressionCommand()
+        {
+            IExpression expr = new AssignExpression("a", new ConstantExpression(1));
+            ExpressionCommand cmd = new ExpressionCommand(expr);
+            Context context = new Context();
+
+            cmd.Execute(context);
+
+            Assert.AreEqual(1, context.GetValue("a"));
+        }
+    }
+}
---
+++ Src/BScript/Commands/ExpressionCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace BScript.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Expressions;
+
+    public class ExpressionCommand
+    {
+        private IExpression expression;
+
+        public ExpressionCommand(IExpression expression)
+        {
+            this.expression = expression;
+        }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public void Execute(Context context)
+        {
+            this.expression.Evaluate(context);
+        }
+    }
+}
