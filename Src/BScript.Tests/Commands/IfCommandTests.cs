f2c581de92107b288767ce04433b90d5181b505d
ajlopez <webmaster@ajlopez.com>
2015-01-15 07:56:24 -0300

    IfCommand first implementation and tests

---
+++ Src/BScript.Tests/Commands/IfCommandTests.cs
@@ -0,0 +1,66 @@
+﻿namespace BScript.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Commands;
+    using BScript.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class IfCommandTests
+    {
+        [TestMethod]
+        public void CreateIfCommand()
+        {
+            IExpression cond = new ConstantExpression(1);
+            ICommand thencmd = new ExpressionCommand(new ConstantExpression(2));
+            IfCommand cmd = new IfCommand(cond, thencmd);
+
+            Assert.IsNotNull(cmd.Condition);
+            Assert.AreSame(cond, cmd.Condition);
+            Assert.IsNotNull(cmd.ThenCommand);
+            Assert.AreSame(thencmd, cmd.ThenCommand);
+        }
+
+        [TestMethod]
+        public void ExecuteIfCommandWithTrueCondition()
+        {
+            Context context = new Context();
+            IExpression cond = new ConstantExpression(true);
+            ICommand thencmd = new ExpressionCommand(new AssignExpression("a", new ConstantExpression(42)));
+            IfCommand cmd = new IfCommand(cond, thencmd);
+
+            cmd.Execute(context);
+
+            Assert.AreEqual(42, context.GetValue("a"));
+        }
+
+        [TestMethod]
+        public void ExecuteIfCommandWithFalseCondition()
+        {
+            Context context = new Context();
+            IExpression cond = new ConstantExpression(false);
+            ICommand thencmd = new ExpressionCommand(new AssignExpression("a", new ConstantExpression(42)));
+            IfCommand cmd = new IfCommand(cond, thencmd);
+
+            cmd.Execute(context);
+
+            Assert.IsNull(context.GetValue("a"));
+        }
+
+        [TestMethod]
+        public void ExecuteIfCommandWithNullCondition()
+        {
+            Context context = new Context();
+            IExpression cond = new ConstantExpression(null);
+            ICommand thencmd = new ExpressionCommand(new AssignExpression("a", new ConstantExpression(42)));
+            IfCommand cmd = new IfCommand(cond, thencmd);
+
+            cmd.Execute(context);
+
+            Assert.IsNull(context.GetValue("a"));
+        }
+    }
+}
---
+++ Src/BScript/Commands/IfCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace BScript.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Expressions;
+
+    public class IfCommand : ICommand
+    {
+        private IExpression condition;
+        private ICommand thencmd;
+
+        public IfCommand(IExpression condition, ICommand thencmd)
+        {
+            this.condition = condition;
+            this.thencmd = thencmd;
+        }
+
+        public IExpression Condition { get { return this.condition; } }
+
+        public ICommand ThenCommand { get { return this.thencmd; } }
+
+        public void Execute(Context context)
+        {
+            if (IsFalse(this.condition.Evaluate(context)))
+                return;
+
+            this.thencmd.Execute(context);
+        }
+
+        private static bool IsFalse(object value)
+        {
+            return value == null || value.Equals(false);
+        }
+    }
+}
