29f314c5319f2658c09e52025860ba07f9f00bf4
ajlopez <webmaster@ajlopez.com>
2015-01-30 07:56:50 -0300

    Parser parse <> compare expression

--- Src/BScript/Compiler/Parser.cs
+++ Src/BScript/Compiler/Parser.cs
@@ -11,7 +11,7 @@
     {
         private static string[][] operators = new string[][]
         {
-            new string[] { "==" },
+            new string[] { "==", "<>" },
             new string[] { "+", "-" },
             new string[] { "*", "/" }
         };
@@ -65,6 +65,8 @@
             {
                 if (token.Value == "==")
                     expr = new BinaryOperatorExpression(BinaryOperator.Equal, expr, this.ParseBinaryExpression(level + 1));
+                if (token.Value == "<>")
+                    expr = new BinaryOperatorExpression(BinaryOperator.NotEqual, expr, this.ParseBinaryExpression(level + 1));
                 if (token.Value == "+")
                     expr = new BinaryOperatorExpression(BinaryOperator.Add, expr, this.ParseBinaryExpression(level + 1));
                 if (token.Value == "-")
