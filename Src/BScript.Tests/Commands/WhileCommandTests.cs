ff704139c4e05a373f0116bd614ce6590b103b7d
ajlopez <webmaster@ajlopez.com>
2015-02-04 09:16:52 -0300

    While command

---
+++ Src/BScript.Tests/Commands/WhileCommandTests.cs
@@ -0,0 +1,55 @@
+﻿namespace BScript.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Commands;
+    using BScript.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class WhileCommandTests
+    {
+        [TestMethod]
+        public void CreateWhileCommand()
+        {
+            IExpression cond = new ConstantExpression(1);
+            ICommand cmd = new ExpressionCommand(new ConstantExpression(2));
+            WhileCommand wcmd = new WhileCommand(cond, cmd);
+
+            Assert.IsNotNull(wcmd.Condition);
+            Assert.AreSame(cond, wcmd.Condition);
+            Assert.IsNotNull(wcmd.Command);
+            Assert.AreSame(cmd, wcmd.Command);
+        }
+
+        [TestMethod]
+        public void ExecuteWhileCommandWithTrueCondition()
+        {
+            Context context = new Context();
+            IExpression cond = new NameExpression("a");
+            ICommand cmd = new ExpressionCommand(new AssignExpression("a", new ConstantExpression(false)));
+            WhileCommand wcmd = new WhileCommand(cond, cmd);
+
+            context.SetValue("a", true);
+
+            wcmd.Execute(context);
+
+            Assert.AreEqual(false, context.GetValue("a"));
+        }
+
+        [TestMethod]
+        public void ExecuteWhileCommandWithNullCondition()
+        {
+            Context context = new Context();
+            IExpression cond = new ConstantExpression(null);
+            ICommand cmd = new ExpressionCommand(new AssignExpression("a", new ConstantExpression(42)));
+            WhileCommand wcmd = new WhileCommand(cond, cmd);
+
+            wcmd.Execute(context);
+
+            Assert.IsNull(context.GetValue("a"));
+        }
+    }
+}
---
+++ Src/BScript/Commands/WhileCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace BScript.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Expressions;
+
+    public class WhileCommand : ICommand
+    {
+        private IExpression condition;
+        private ICommand cmd;
+
+        public WhileCommand(IExpression condition, ICommand cmd)
+        {
+            this.condition = condition;
+            this.cmd = cmd;
+        }
+
+        public IExpression Condition { get { return this.condition; } }
+
+        public ICommand Command { get { return this.cmd; } }
+
+        public void Execute(Context context)
+        {
+            while (!IsFalse(this.condition.Evaluate(context)))
+                this.cmd.Execute(context);
+        }
+
+        private static bool IsFalse(object value)
+        {
+            return value == null || value.Equals(false);
+        }
+    }
+}
