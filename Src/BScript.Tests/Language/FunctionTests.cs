955e4e47b60a5e14f237fc680f574947ba6afa8c
ajlopez <webmaster@ajlopez.com>
2015-02-22 09:53:42 -0300

    Function first tests

---
+++ Src/BScript.Tests/Language/FunctionTests.cs
@@ -0,0 +1,38 @@
+﻿namespace BScript.Tests.Language
+{
+    using System;
+    using BScript.Commands;
+    using BScript.Expressions;
+    using BScript.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FunctionTests
+    {
+        [TestMethod]
+        public void CreateFunction()
+        {
+            ICommand body = new ExpressionCommand(new ConstantExpression(1));
+            Function fn = new Function(new string[] { "a", "b" }, body);
+
+            Assert.IsNotNull(fn.ArgumentNames);
+            Assert.AreEqual(2, fn.ArgumentNames.Count);
+            Assert.AreEqual("a", fn.ArgumentNames[0]);
+            Assert.AreEqual("b", fn.ArgumentNames[1]);
+            Assert.IsNotNull(fn.Body);
+            Assert.AreSame(body, fn.Body);
+        }
+
+        [TestMethod]
+        public void ExecuteFunction()
+        {
+            ICommand body = new ReturnCommand(new BinaryOperatorExpression(BinaryOperator.Add, new NameExpression("a"), new NameExpression("b")));
+            Function fn = new Function(new string[] { "a", "b" }, body);
+
+            var result = fn.Evaluate(new object[] { 1, 2 });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ Src/BScript/Language/Function.cs
@@ -0,0 +1,40 @@
+﻿namespace BScript.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Commands;
+
+    public class Function
+    {
+        private IList<string> argnames;
+        private ICommand body;
+
+        public Function(IList<string> argnames, ICommand body)
+        {
+            this.argnames = argnames;
+            this.body = body;
+        }
+
+        public IList<string> ArgumentNames { get { return this.argnames; } }
+
+        public ICommand Body { get { return this.body; } }
+
+        public object Evaluate(IList<object> values)
+        {
+            Context context = new Context();
+
+            for (int k = 0; k < argnames.Count; k++)
+                context.SetValue(argnames[k], values[k]);
+
+            this.body.Execute(context);
+
+            if (context.HasReturn)
+                return context.ReturnValue;
+            else
+                return null;
+        }
+    }
+}
+
