f190cd7390d20364ec7d4589a41d0bf12cca2c19
ajlopez <webmaster@ajlopez.com>
2015-02-25 15:50:28 -0300

    Function command, first test

---
+++ Src/BScript.Tests/Commands/FunctionCommandTests.cs
@@ -0,0 +1,28 @@
+﻿namespace BScript.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Commands;
+    using BScript.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FunctionCommandTests
+    {
+        [TestMethod]
+        public void CreateFunctionCommand()
+        {
+            string name = "foo";
+            IList<string> argnames = new string[] { "a", "b" };
+            ICommand body = new ExpressionCommand(new ConstantExpression(2));
+
+            FunctionCommand fcmd = new FunctionCommand(name, argnames, body);
+
+            Assert.AreEqual(name, fcmd.Name);
+            Assert.AreSame(argnames, fcmd.ArgumentNames);
+            Assert.AreSame(body, fcmd.Body);
+        }
+    }
+}
---
+++ Src/BScript/Commands/FunctionCommand.cs
@@ -0,0 +1,33 @@
+﻿namespace BScript.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Expressions;
+
+    public class FunctionCommand : ICommand
+    {
+        private string name;
+        private IList<string> argnames;
+        private ICommand body;
+
+        public FunctionCommand(string name, IList<string> argnames, ICommand body)
+        {
+            this.name = name;
+            this.argnames = argnames;
+            this.body = body;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IList<string> ArgumentNames { get { return this.argnames; } }
+
+        public ICommand Body { get { return this.body; } }
+
+        public void Execute(Context context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
