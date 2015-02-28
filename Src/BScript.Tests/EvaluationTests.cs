7a7c59286884ad3bb7fe0fc2559b3a5ad947ce0f
ajlopez <webmaster@ajlopez.com>
2015-02-28 07:48:54 -0300

    Additional test: evaluate number

---
+++ Src/BScript.Tests/EvaluationTests.cs
@@ -0,0 +1,29 @@
+﻿namespace BScript.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BScript.Compiler;
+
+    [TestClass]
+    public class EvaluationTests
+    {
+        [TestMethod]
+        public void EvaluateNumber()
+        {
+            var result = EvaluateExpression("42");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(42, result);
+        }
+
+        private static object EvaluateExpression(string text)
+        {
+            var parser = new Parser(text);
+            var expr = parser.ParseExpression();
+            return expr.Evaluate(null);
+        }
+    }
+}
