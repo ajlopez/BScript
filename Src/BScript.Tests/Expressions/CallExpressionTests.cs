f4bb25a4f6c5cb617c607c302263c4d48d3d1707
ajlopez <webmaster@ajlopez.com>
2015-03-07 08:25:38 -0300

    Create call expression

---
+++ Src/BScript.Tests/Expressions/CallExpressionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace BScript.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BScript.Expressions;
+
+    [TestClass]
+    public class CallExpressionTests
+    {
+        [TestMethod]
+        public void CreateCallExpression()
+        {
+            IList<IExpression> exprs = new List<IExpression>() { new ConstantExpression(1), new ConstantExpression(2) };
+
+            var expr = new CallExpression("foo", exprs);
+
+            Assert.AreEqual("foo", expr.Name);
+            Assert.AreSame(exprs, expr.ArgumentExpressions);
+        }
+    }
+}
---
+++ Src/BScript/Expressions/CallExpression.cs
@@ -0,0 +1,28 @@
+﻿namespace BScript.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class CallExpression : BScript.Expressions.IExpression
+    {
+        private string name;
+        private IList<IExpression> argexprs;
+
+        public CallExpression(string name, IList<IExpression> argexprs)
+        {
+            this.name = name;
+            this.argexprs = argexprs;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IList<IExpression> ArgumentExpressions { get { return this.argexprs; } }
+
+        public object Evaluate(Context context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
