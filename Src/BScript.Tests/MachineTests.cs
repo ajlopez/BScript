635b302d6c99e510f8788e029183c6319e2cf80f
ajlopez <webmaster@ajlopez.com>
2015-03-09 08:53:27 -0300

    Machine first test

---
+++ Src/BScript.Tests/MachineTests.cs
@@ -0,0 +1,17 @@
+﻿namespace BScript.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MachineTests
+    {
+        [TestMethod]
+        public void CreateWithRootContext()
+        {
+            var machine = new Machine();
+
+            Assert.IsNotNull(machine.RootContext);
+        }
+    }
+}
---
+++ Src/BScript/Machine.cs
@@ -0,0 +1,19 @@
+﻿namespace BScript
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Machine
+    {
+        private Context context;
+
+        public Machine()
+        {
+            this.context = new Context();
+        }
+
+        public Context RootContext { get { return this.context; } }
+    }
+}
