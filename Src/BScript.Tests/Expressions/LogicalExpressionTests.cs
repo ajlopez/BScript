522e3f0f4929c10151254c92c36cdf00a5bcc5b6
ajlopez <webmaster@ajlopez.com>
2015-03-21 14:39:33 -0300

    Or expression, first test

---
+++ Src/BScript.Tests/Expressions/LogicalExpressionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BScript.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BScript.Expressions;
+
+    [TestClass]
+    public class LogicalExpressionTests
+    {
+        [TestMethod]
+        public void OrFalseFalse()
+        {
+            var lexpr = new ConstantExpression(false);
+            var rexpr = new ConstantExpression(false);
+            var expr = new OrExpression(lexpr, rexpr);
+
+            Assert.AreSame(lexpr, expr.LeftExpression);
+            Assert.AreSame(rexpr, expr.RightExpression);
+
+            Assert.AreEqual(false, expr.Evaluate(null));
+        }
+    }
+}
---
+++ Src/BScript/Expressions/OrExpression.cs
@@ -0,0 +1,32 @@
+﻿namespace BScript.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualBasic.CompilerServices;
+
+    public class OrExpression : IExpression
+    {
+        private IExpression left;
+        private IExpression right;
+
+        public OrExpression(IExpression left, IExpression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public IExpression LeftExpression { get { return this.left; } }
+
+        public IExpression RightExpression { get { return this.right; } }
+
+        public object Evaluate(Context context)
+        {
+            if (false.Equals(this.left.Evaluate(context)))
+                return false;
+
+            return true;
+        }
+    }
+}
