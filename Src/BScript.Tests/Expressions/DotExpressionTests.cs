293e10c5bcf9ef7b203411a0ba52c9f3669752c7
ajlopez <webmaster@ajlopez.com>
2015-04-01 17:18:53 -0300

    Dot expression, WIP

---
+++ Src/BScript.Tests/Expressions/DotExpressionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BScript.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Commands;
+    using BScript.Expressions;
+    using BScript.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DotExpressionTests
+    {
+        [TestMethod]
+        public void CreateDotExpression()
+        {
+            IExpression lexpr = new ConstantExpression(1);
+
+            var expr = new DotExpression(lexpr, "Foo");
+
+            Assert.AreEqual("Foo", expr.Name);
+            Assert.AreSame(lexpr, expr.Expression);
+        }
+    }
+}
---
+++ Src/BScript/Expressions/DotExpression.cs
@@ -0,0 +1,29 @@
+﻿namespace BScript.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Language;
+
+    public class DotExpression : IExpression
+    {
+        private IExpression expression;
+        private string name;
+
+        public DotExpression(IExpression expression, string name)
+        {
+            this.expression = expression;
+            this.name = name;
+        }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public string Name { get { return this.name; } }
+
+        public object Evaluate(Context context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
