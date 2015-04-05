734f72c7ebcf4b44519f029f134d1e9772f8577f
ajlopez <webmaster@ajlopez.com>
2015-04-05 08:36:36 -0300

    Call dot expression, WIP

---
+++ Src/BScript.Tests/Expressions/CallDotExpressionTests.cs
@@ -0,0 +1,27 @@
+﻿namespace BScript.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Commands;
+    using BScript.Expressions;
+    using BScript.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CallDotExpressionTests
+    {
+        [TestMethod]
+        public void CreateCallDotExpression()
+        {
+            NameExpression nexpr = new NameExpression("foo");
+            IList<IExpression> exprs = new List<IExpression>() { new ConstantExpression(1), new ConstantExpression(2) };
+
+            var expr = new CallDotExpression(nexpr, exprs);
+
+            Assert.AreEqual(nexpr, expr.Expression);
+            Assert.AreSame(exprs, expr.ArgumentExpressions);
+        }
+    }
+}
---
+++ Src/BScript/Expressions/CallDotExpression.cs
@@ -0,0 +1,34 @@
+﻿namespace BScript.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BScript.Language;
+
+    public class CallDotExpression : IExpression
+    {
+        private DotExpression expression;
+        private IList<IExpression> argexprs;
+
+        public CallDotExpression(DotExpression expression, IList<IExpression> argexprs)
+        {
+            this.expression = expression;
+            this.argexprs = argexprs;
+        }
+
+        public DotExpression Expression { get { return this.expression; } }
+
+        public IList<IExpression> ArgumentExpressions { get { return this.argexprs; } }
+
+        public object Evaluate(Context context)
+        {
+            IList<object> args = new List<object>();
+
+            foreach (var expr in this.argexprs)
+                args.Add(expr.Evaluate(context));
+
+            throw new NotImplementedException();
+        }
+    }
+}
